0abe6133b16b1167815f462938357575a67640fb
587900 <587900@stud.hvl.no>
2021-02-12 13:09:28 +0100

    Ekstra del til testUtvid

--- JAVA_INNLEVERING-DAT102-oblig1/Test/no/hvl/dat102/sirkulaer/SirkulaerKoeTest.java
+++ JAVA_INNLEVERING-DAT102-oblig1/Test/no/hvl/dat102/sirkulaer/SirkulaerKoeTest.java
@@ -1,6 +1,7 @@
 package no.hvl.dat102.sirkulaer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -28,11 +29,14 @@ public class SirkulaerKoeTest extends KoeADTTest {
 		koe.innKoe(e1);
 		koe.innKoe(e2);
 		koe.innKoe(e3);
+		assertEquals("utvid feil størrelse innKoe", 103, koe.antall());
 		
 		for (int i = 0; i < 100; ++i) assertEquals("loop utvid fail utKoe != e0", e0, koe.utKoe());
 		assertEquals("utvid fail utKoe != e1", e1, koe.utKoe());
 		assertEquals("utvid fail utKoe != e2", e2, koe.utKoe());
 		assertEquals("utvid fail utKoe != e3", e3, koe.utKoe());
+		assertEquals("utvid feil størrelse utKoe", 0, koe.antall());
+		assertTrue("utvid feil erTom", koe.erTom());
 	}
 	
 }
